0976aaebcef1632962f767dd9d466d5d99971c3f
Julio Jimenez <julio@julioj.com>
2022-09-23 19:56:23 -0400

    OpenLayersMap component
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import OpenLayersMap from './components/OpenLayersMap'
 
 const App = () => {
   return (
-    <div>
-      <p>GeoIntel</p>
-    </div>
+    <OpenLayersMap />
   )
 }
 
