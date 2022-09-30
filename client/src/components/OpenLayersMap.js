d09aacd1a8221ef56b6c3722672347f7016dc89b
Julio Jimenez <julio@julioj.com>
2022-09-30 12:14:29 -0400

    Fix Mouse Coordinates
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- client/src/components/OpenLayersMap.js
+++ client/src/components/OpenLayersMap.js
@@ -6,21 +6,21 @@ import TileLayer from 'ol/layer/Tile'
 import VectorLayer from 'ol/layer/Vector'
 import VectorSource from 'ol/source/Vector'
 import XYZ from 'ol/source/XYZ'
-import { transform } from 'ol/proj'
-import { toStringXY } from 'ol/coordinate'
+// import { transform } from 'ol/proj'
+// import { toStringXY } from 'ol/coordinate'
 
 const OpenLayersMap = (props) => {
   const [map, setMap] = useState()
   const [featuresLayer, setFeaturesLayer] = useState()
-  const [selectedCoord, setSelectedCoord] = useState()
+  // const [selectedCoord, setSelectedCoord] = useState()
   const mapElement = useRef()
   mapElement.current = map
 
-  const handleMapClick = (event) => {
-    const clickedCoord = mapElement.current.getCoordinateFromPixel(event.pixel)
-    const transormedCoord = transform(clickedCoord, 'EPSG:3857', 'EPSG:4326')
-    setSelectedCoord(transormedCoord)
-  }
+  // const handleMapClick = (event) => {
+  //   const clickedCoord = mapElement.current.getCoordinateFromPixel(event.pixel)
+  //   const transormedCoord = transform(clickedCoord, 'EPSG:3857', 'EPSG:4326')
+  //   setSelectedCoord(transormedCoord)
+  // }
 
   useEffect(() => {
     // create and add vector source layer
@@ -51,28 +51,25 @@ const OpenLayersMap = (props) => {
 
     setMap(initialMap)
     setFeaturesLayer(initalFeaturesLayer)
-    initialMap.on('click', handleMapClick)
+    // initialMap.on('click', handleMapClick)
   }, [])
 
   useEffect(() => {
     if (props.features.length) {
       featuresLayer.setSource(
         new VectorSource({
-          features: props.features // make sure features is an array
+          features: props.features
         })
       )
-      // fit map to feature extent (with 100px of padding)
-      map.getView().fit(featuresLayer.getSource().getExtent(), {
-        // padding: [100, 100, 100, 100]
-      })
+      map.getView().fit(featuresLayer.getSource().getExtent(), {})
     }
   }, [props.features])
 
   return (
     <div className="container-fluid" style={{ height: '100vh', padding: 0, margin: 0, overflow: 'hidden' }} ref={mapElement}>
-      <div>
+      {/* <div>
         <p style={{ position: 'absolute', right: 5, bottom: 0, zIndex: 1 }}>{ (selectedCoord) ? toStringXY(selectedCoord, 5) : '' }</p>
-      </div>
+      </div> */}
     </div>
   )
 }
